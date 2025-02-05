77db5840da9db1162f6cb600cd4c32a25bee7c95
Rogério Alves <rogerio.alves651@gmail.com>
2025-02-05 16:45:42 -0300

    Adiciona mensagem de boas-vindas adicional ao componente App.

--- meu_blog/src/App.js
+++ meu_blog/src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>Meu Blog</h1>
       <h2>Seja bem-vindo ao meu blog!</h2>
+      <h3>Espero que você goste dos meus posts.</h3>
     </div>
   );
 }
