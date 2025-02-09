6fff11366126140040d4fa74ea52744f389e2bc8
Rogério <rogerio.alves651@gmail.com>
2025-02-08 23:24:04 -0300

    Adiciona novos contêineres e ajusta estilos no CSS.

--- meu_blog/src/App.js
+++ meu_blog/src/App.js
@@ -7,10 +7,26 @@ function App() {
       <h2>Seja bem-vindo ao meu blog!</h2>
       <h3>Espero que você goste dos meus posts.</h3>
       
-      <div className="SecondContainer">
-        <h2>Segundo Contêiner</h2>
-        <p>Este é o conteúdo do segundo contêiner.</p>
-      </div>
+      <SecondContainer />
+      <ThirdContainer />
+    </div>
+  );
+}
+
+function SecondContainer() {
+  return (
+    <div className="SecondContainer">
+      <h2>Segundo Contêiner</h2>
+      <p>Este é o conteúdo do segundo contêiner.</p>
+    </div>
+  );
+}
+
+function ThirdContainer() {
+  return (
+    <div className="ThirdContainer">
+      <h2>Terceiro Contêiner</h2>
+      <p>Este é o conteúdo do terceiro contêiner.</p>
     </div>
   );
 }
