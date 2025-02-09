00148132f504ff574b88956337e33c7b5999aad8
Rogério <rogerio.alves651@gmail.com>
2025-02-08 23:13:14 -0300

    Adiciona segundo contêiner com estilo de fundo azul claro.

--- meu_blog/src/App.js
+++ meu_blog/src/App.js
@@ -6,6 +6,11 @@ function App() {
       <h1>Meu Blog</h1>
       <h2>Seja bem-vindo ao meu blog!</h2>
       <h3>Espero que você goste dos meus posts.</h3>
+      
+      <div className="SecondContainer">
+        <h2>Segundo Contêiner</h2>
+        <p>Este é o conteúdo do segundo contêiner.</p>
+      </div>
     </div>
   );
 }
