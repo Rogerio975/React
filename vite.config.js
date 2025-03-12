f08b4e31a528eb377bfc7f638e28e117402db45b
Rogério Alves <rogerio.alves651@gmail.com>
2025-03-12 11:09:05 -0300

    Configura projeto com Vite, React e TypeScript, adicionando arquivos de configuração e dependências iniciais.

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000, // Opcional, define a porta padrão
+  }
+});
