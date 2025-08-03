7f8b66d1902f0cc75553a30e9c55fab72abd3e4e
Rogerio <rogerio.alves651@gmail.com>
2025-08-02 22:47:00 -0300

    feat: add Header component and update App structure

--- introducao-react/src/App.jsx
+++ introducao-react/src/App.jsx
@@ -1,15 +1,14 @@
 import './App.css';
 import logo from './assets/react.svg';
+import Header from './Header';
  
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       <h1>Hello World!</h1>
-      </header>
+      <Header />
+      <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
 }
- 
+
 export default App;
