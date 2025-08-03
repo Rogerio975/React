44cf6d485cffdf9b3cc57e0e7945bf9abd7e2e2c
Rogerio <rogerio.alves651@gmail.com>
2025-08-02 23:35:55 -0300

    Arquivo App.jsx restaurado.

--- introducao-react/src/App.jsx
+++ introducao-react/src/App.jsx
@@ -1,11 +1,25 @@
-
+import { useState } from 'react';
 import './App.css';
-import Home from './pages/Home.jsx';
-
+import logo from './assets/react.svg';
+import Header from './Header';
+ 
 function App() {
+  const [contador, setContador] = useState(0);
+  function increment(){
+    setContador(contador + 1);
+    console.log(contador);
+  }
+ 
   return (
-    <Home />
-  )
+    <div className="App">
+      <Header title="Header Param" />
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+      <input type="button" value="Clique" onClick={increment} />
+      </p>
+      <p>{contador}</p> cliques!
+    </div>
+  );
 }
 
 export default App;
