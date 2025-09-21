cd594c27321036e083cb4fd1c98cec3bd7257068
Rogério Alves <rogerio.alves651@gmail.com>
2025-09-21 00:23:14 -0300

    Projeto alterado.

--- rotas/src/App.jsx
+++ rotas/src/App.jsx
@@ -1,25 +1,28 @@
-import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import Contato from './pages/Contato'
-import Home from './pages/Home'
-import Sobre from './pages/Sobre'
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css'; // Importe o arquivo CSS
+import Contato from './pages/Contato';
+import Home from './pages/Home';
+import Sobre from './pages/Sobre';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sobre" element={<Sobre />} />
-        <Route path="/contato" element={<Contato />} />
-      </Routes>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/sobre">Sobre</Link></li>
-          <li><Link to="/contato">Contato</Link></li>
-        </ul>
-      </nav>
+      <div className="main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/contato" element={<Contato />} />
+        </Routes>
+        <nav>
+          <ul>
+            <Link to="/">Home</Link>
+            <Link to="/sobre">Sobre</Link>
+            <Link to="/contato">Contato</Link>
+          </ul>
+        </nav>
+      </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
