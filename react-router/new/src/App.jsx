6e3a9cba610509418e977c4b9071ba648bb747cd
Rogerio Alves <rogerio.alves651@gmail.com>
2025-11-05 13:45:10 -0300

    Projeto "new" criado e o o outro projeto foi movido para a pasta "old".

---
+++ react-router/new/src/App.jsx
@@ -0,0 +1,40 @@
+// App.jsx
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <header className="header">
+        <h1 className="title">Página Inicial</h1>
+        <nav className="nav">
+          <Link to="/">Home</Link>
+          <Link to="/sobre">Sobre</Link>
+          <Link to="/contato">Contato</Link>
+        </nav>
+      </header>
+
+      <main className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/contato" element={<Contato />} />
+        </Routes>
+      </main>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Bem-vindo à Home!</h2>;
+}
+
+function Sobre() {
+  return <h2>Sobre nós</h2>;
+}
+
+function Contato() {
+  return <h2>Entre em contato</h2>;
+}
+
+export default App;
