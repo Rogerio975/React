5a4342cc6fa17a4721fea62fc47226fce13e32ae
Rogerio Alves <rogerio.alves651@gmail.com>
2025-11-05 11:48:25 -0300

    Projeto criado.

---
+++ react-router/src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Início</Link> | <Link to="/about">Sobre</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
