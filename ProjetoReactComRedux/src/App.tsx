5802464709bd73a79dfa3bc9c14cbb385f5159b1
Rogerio Alves <rogerio.alves651@gmail.com>
2025-12-23 20:49:29 -0300

    feat: initialize React project with Redux for state management
    - Added package.json with dependencies for React, Redux, and Vite.
    - Included Vite logo SVG in public directory.
    - Created App.css for styling the application.
    - Developed App.tsx component with increment and decrement functionality using Redux.
    - Added React logo SVG in assets directory.
    - Implemented contadorSlice for managing counter state with increment and decrement actions.
    - Set up global styles in index.css.
    - Configured main.tsx to render the App component with Redux Provider.
    - Established Redux store in store.ts with contador reducer.
    - Configured TypeScript settings in tsconfig files for app and node.
    - Created Vite configuration file for project setup.

---
+++ ProjetoReactComRedux/src/App.tsx
@@ -0,0 +1,17 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { decrementar, incrementar } from './features/contadorSlice';
+
+function App() {
+  const valor = useSelector((state) => state.contador.valor);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>Contador: {valor}</h1>
+      <button onClick={() => dispatch(incrementar())}>+</button>
+      <button onClick={() => dispatch(decrementar())}>-</button>
+    </div>
+  );
+}
+
+export default App;
